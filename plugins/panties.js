f99e2850f515d59aa8880745548276f38a05e54d
imZemo <106836635+imZemo@users.noreply.github.com>
2022-06-07 13:06:34 -0500

    Update panties.js

--- plugins/panties.js
+++ plugins/panties.js
@@ -6,6 +6,6 @@ let json = res.data
 let url = json.url
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let mentionedJid = [who]
-conn.send2ButtonImg(m.chat, url, "*Panties*", '©The Shadow Borkers - Bot', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }}) }
+conn.send2ButtonImg(m.chat, url, "*Panties*", '©𝐔𝐧 𝐒𝐢𝐦𝐩𝐥𝐞 𝐁𝐨𝐭𝐬𝐢𝐭𝐨 𓃵', '🥵 SIGUIENTE 🥵', `${usedPrefix + command}`, '🔥 LABIBLIA 🔥', `${usedPrefix}labiblia`, m, false, { contextInfo: { mentionedJid }}) }
 handler.command = /^(panties)$/i
 module.exports = handler
