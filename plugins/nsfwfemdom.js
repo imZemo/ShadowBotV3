fa3038ebb9c5ce41eeecd7964d04faa8d1c7cb4d
imZemo <106836635+imZemo@users.noreply.github.com>
2022-06-08 01:18:59 -0500

    Update nsfwfemdom.js

--- plugins/nsfwfemdom.js
+++ plugins/nsfwfemdom.js
@@ -3,7 +3,7 @@ let handler  = async (m, { conn, usedPrefix, command }) => {
 if (!DATABASE._data.chats[m.chat].nsfw && m.isGroup) throw '*[ ⚠️ ] Los comandos +18 estan desactivados en este grupo, si es administrador de este grupo y desea activarlos escriba #enable nsfw*'
 heum = await fetch(`https://server-api-rey.herokuapp.com/api/nsfw/femdom?apikey=apirey`)
 json = await heum.buffer()
-conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©The Shadow Brokers - Bot', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
+conn.sendButtonImg(m.chat, json, '*Disfrutalo!!*', '©𝐔𝐧 𝐒𝐢𝐦𝐩𝐥𝐞 𝐁𝐨𝐭𝐬𝐢𝐭𝐨 𓃵', 'SIGUIENTE', `${usedPrefix + command}`, m, false)
 }
 handler.command = /^(femdom|nsfwfemdom)$/i
 module.exports = handler
